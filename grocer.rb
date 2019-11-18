e6bf764c6f02d5211d5556c678b8c8ad844c65e7
Allen Zhang <azhang9328@gmail.com>
2019-11-18 07:01:37 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -71,7 +71,6 @@ def apply_clearance(cart)
   #
   # REMEMBER: This method **should** update cart
   origCartIndex = 0 
-  puts cart
   puts cart.length
   while origCartIndex < cart.length do 
     puts "loopy"  
